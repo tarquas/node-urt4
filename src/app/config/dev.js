3bbb0f653f36a3fd865586c690bf5174518833a9
Taras Mykhailovych <tarquas@gmail.com>
2019-03-02 12:56:35 +0100

    v.0.1.0: all goes open source; fix config 2

--- src/app/config/dev.js
+++ src/app/config/dev.js
@@ -3,6 +3,5 @@ module.exports = {
   db: {connString: 'mongodb://127.0.0.1:27017/dev-nodeurt-mod'},
   ip: {accessKey: '37d10b37059554b6a20e2430b26640c2'},
   modes: require('./modes'),
-  verb: process.env.VERB,
-  rules: require('./rules')
+  verb: process.env.VERB
 };
